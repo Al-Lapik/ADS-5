ba517cb1496cf8cc7b357228638f765e830e442f
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-03-25 21:43:56 +0300

    B178 Лапихин Александр

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,117 @@
 #include <map>
 #include "tstack.h"
 
+int prior(char operation) {
+  if (operation == '*' || operation == '/')
+    return 2;
+  if (operation == '+' || operation == '-')
+    return 1;
+  if (operation == '(')
+    return 0;
+  return -1;
+}
+
+int convert(char num) {
+  int analogue = 0;
+  if (num >= '0' && num <= '9')
+    for (char i = '0'; i <= '9'; i++, analogue++)
+      if (num == i)
+        return analogue;
+  return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string pstf = "";
+  for (int i = 0; i < inf.size(); i++) {
+    if (prior(inf[i]) == -1) {
+      if (inf[i] == ')') {
+        while (prior(stack1.get()) != 0) {
+          if (pstf[pstf.size() - 1] != ' ')
+            pstf += " ";
+          pstf += stack1.get();
+          pstf += " ";
+          stack1.pop();
+        }
+        stack1.pop();
+        continue;
+      }
+      pstf += inf[i];
+      if (prior(inf[i + 1]) != -1)
+        pstf += " ";
+    }
+    else if (stack1.isEmpty() || prior(inf[i]) == 0 ||
+             prior(inf[i]) > prior(stack1.get()))
+      stack1.push(inf[i]);
+    else if (prior(inf[i]) <= prior(stack1.get())) {
+      if (pstf[pstf.size() - 1] != ' ')
+        pstf += " ";
+      pstf += stack1.get();
+      pstf += " ";
+      stack1.pop();
+      stack1.push(inf[i]);
+    }
+  }
+  while (!stack1.isEmpty()) {
+    if (pstf[pstf.size() - 1] != ' ')
+      pstf += " ";
+    pstf += stack1.get();
+    pstf += " ";
+    stack1.pop();
+  }
+  return pstf.substr(0, pstf.size() - 1);
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  int operand = 0;
+  for (int i = 0; i < post.size(); i++) {
+    int current = convert(post[i]);
+    if (current > -1)
+      operand = operand * 10 + current;
+    else {
+      if (operand != 0) {
+        stack2.push(operand);
+        operand = 0;
+      }
+      switch (post[i])
+      {
+        case '+':
+          {
+            int oper2 = stack2.get();
+            stack2.pop();
+            int oper1 = stack2.get();
+            stack2.pop();
+            stack2.push(oper1 + oper2);
+            break;
+          }
+        case '-':
+          {
+            int oper2 = stack2.get();
+            stack2.pop();
+            int oper1 = stack2.get();
+            stack2.pop();
+            stack2.push(oper1 - oper2);
+            break;
+          }
+        case '*':
+          {
+            int oper2 = stack2.get();
+            stack2.pop();
+            int oper1 = stack2.get();
+            stack2.pop();
+            stack2.push(oper1 * oper2);
+            break;
+          }
+        case '/':
+          {
+            int oper2 = stack2.get();
+            stack2.pop();
+            int oper1 = stack2.get();
+            stack2.pop();
+            stack2.push(oper1 / oper2);
+            break;
+          }
+      }
+    }
+  }
+  return stack2.get();
 }
